b036afcfadf47031464052b06b711132e829cd58
Matthew Ung <matthew.ung814@gmail.com>
2024-02-25 18:19:57 -0800

    Update song search limit to 50

--- lib/controls.py
+++ lib/controls.py
@@ -135,7 +135,8 @@ def search(token, query, params={}):
 
 def search_song(token, query):
     params = {
-            'type': 'track'
+            'type': 'track',
+            'limit': 50
             }
 
     resp = search(token, query, params)
