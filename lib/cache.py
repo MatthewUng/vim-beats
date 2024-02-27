28e7569e262af29ddb4b62fe8b6abeafc81cb8a5
Matthew Ung <matthew.ung814@gmail.com>
2024-02-26 21:24:29 -0800

    Update cache

--- lib/cache.py
+++ lib/cache.py
@@ -1,10 +1,15 @@
 import os
 import os.path
 from datetime import datetime, timedelta
+import tempfile
+
+def _get_abs_path(fname):
+     return os.path.join(tempfile.gettempdir(), fname)
 
 def get_with_ttl(fname, ttl_seconds):
+    abs_path = _get_abs_path(fname)
     try:
-        s = os.stat(fname)
+        s = os.stat(abs_path)
     except FileNotFoundError:
         return None
 
@@ -12,9 +17,10 @@ def get_with_ttl(fname, ttl_seconds):
     if datetime.now() - m_time > timedelta(seconds=ttl_seconds):
         return None
 
-    with open(fname, 'r') as f:
+    with open(abs_path, 'r') as f:
         return f.read()
 
 def write(fname, contents):
-    with open(fname, 'w') as f:
+    abs_path = _get_abs_path(fname)
+    with open(abs_path, 'w') as f:
         f.write(contents)
