eab7f9f669d1499afff79a192886898589f6afe8
Matthew Ung <matthew.ung814@gmail.com>
2024-03-26 00:13:02 -0700

    Update reload script with correct dir

--- scripts/reload_query.sh
+++ scripts/reload_query.sh
@@ -3,5 +3,8 @@ set -x
 NEW_QUERY=$1
 QUERY_RESULTS_FILE=$2
 
-python3 script.py search --query "$NEW_QUERY" > $QUERY_RESULTS_FILE
-python3 scripts/track_names.py < $QUERY_RESULTS_FILE
+SCRIPT_DIR=$(dirname $0)
+ROOT_DIR=$(dirname $SCRIPT_DIR)
+
+python3 $ROOT_DIR/script.py search --query "$NEW_QUERY" > $QUERY_RESULTS_FILE
+python3 $SCRIPT_DIR/track_names.py < $QUERY_RESULTS_FILE
