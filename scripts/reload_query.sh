92b0d4886f7708e149706199cdb4f03b5ca76b74
Matthew Ung <matthew.ung814@gmail.com>
2024-04-10 15:37:14 -0700

    Fix reload bug

--- scripts/reload_query.sh
+++ scripts/reload_query.sh
@@ -6,5 +6,5 @@ QUERY_RESULTS_FILE=$2
 SCRIPT_DIR=$(dirname $0)
 ROOT_DIR=$(dirname $SCRIPT_DIR)
 
-python3 $ROOT_DIR/script.py search --query "$NEW_QUERY" > $QUERY_RESULTS_FILE
+python3 $ROOT_DIR/script.py search-song --query "$NEW_QUERY" > $QUERY_RESULTS_FILE
 python3 $SCRIPT_DIR/track_names.py < $QUERY_RESULTS_FILE
